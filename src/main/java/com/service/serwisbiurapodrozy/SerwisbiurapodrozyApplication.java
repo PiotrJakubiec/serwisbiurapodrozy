cbfe8bd00cdaeb5aa859b20c88ac84c3e6790dd1
PiotrJakubiec <67845426+PiotrJakubiec@users.noreply.github.com>
2021-03-07 10:02:31 +0100

    Initial commit

---
+++ src/main/java/com/service/serwisbiurapodrozy/SerwisbiurapodrozyApplication.java
@@ -0,0 +1,13 @@
+package com.service.serwisbiurapodrozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisbiurapodrozyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisbiurapodrozyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/serwisbiurapodrozy/SerwisbiurapodrozyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.serwisbiurapodrozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisbiurapodrozyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
