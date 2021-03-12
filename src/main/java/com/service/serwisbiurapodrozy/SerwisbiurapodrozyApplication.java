1b135f0a98df0bb11fe36cbd325af3939f58a193
PiotrJakubiec <67845426+PiotrJakubiec@users.noreply.github.com>
2021-03-12 22:25:32 +0100

    1-szy commit

--- src/main/java/com/service/serwisbiurapodrozy/SerwisbiurapodrozyApplication.java
+++ src/main/java/com/service/serwisbiurapodrozy/SerwisbiurapodrozyApplication.java
@@ -2,6 +2,22 @@ package com.service.serwisbiurapodrozy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+class MainController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+public String index(Model model){
+    model.addAttribute("obj", new Object());
+        return "index";
+}
+
+}
 
 @SpringBootApplication
 public class SerwisbiurapodrozyApplication {
